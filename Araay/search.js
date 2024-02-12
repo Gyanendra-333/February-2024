c61861f77fae74ad1e02bea7804dce14730fcf7b
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-02-12 21:40:48 +0530

    search

---
+++ Araay/search.js
@@ -0,0 +1,10 @@
+let data = [1, 2, 3, 4, 5, 6, 7];
+let searchItem = 4;
+let index;
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i] === searchItem) {
+        index = i;
+    }
+}
+console.log(index);
