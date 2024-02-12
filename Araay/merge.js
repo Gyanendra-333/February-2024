fe882e55832d4d37529d6589c150ee45b1098a00
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-02-12 21:56:02 +0530

    merge Array

--- Araay/merge.js
+++ Araay/merge.js
@@ -9,3 +9,8 @@ for (let i = 0; i < data2.length; i++) {
     data3[data1.length + i] = data2[i]
 }
 console.log(data3);
+
+// 2nd method merge two Array 
+
+let data4 = [[...data1] + "," + [...data2]];
+console.log(data4);
