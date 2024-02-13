03d47c34948c02a2f89fa31c5b03722e50d65b4e
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-02-13 21:16:50 +0530

    logic

--- Araay/sorting.js
+++ Araay/sorting.js
@@ -2,6 +2,10 @@ let data = [10, 20, 50, 30, 80, 40, 90, 70];
 
 for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < data.length; j++) {
-        console.log(j);
+        if (data[j] > data[j + 1]) {
+            let temp = data[j];
+            data[j] = data[j + 1];
+            data[j + 1] = temp;
+        }
     }
 }
