c01c8b376444cda136f4da4717bd0217766c3584
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-02-14 20:54:59 +0530

    factorial

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -2,10 +2,10 @@ function fact(n) {
 
     // Base Or start condition 
     if (n == 0) {
-        return 1;
+        return 1; // return 5 4 3 2 1
     }
     // Recusive call
-    return n * fact(n - 1);
+    return n * fact(n - 1); // return 5*4*3*2*1 => 120
 }
 let data = 5;
 console.log(fact(data));
