46f1e408a7cd797d4b4b2c4232c42b9d9c9c0435
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-02-17 21:32:35 +0530

    pop method

--- Stack/push-pop.js
+++ Stack/push-pop.js
@@ -2,6 +2,7 @@ let data = [];
 let currentSize = data.length;
 let maxSize = 5;
 
+// Add New Element in Last 
 function push(newVal) {
     if (currentSize >= maxSize) {
         console.log("Stack is Already Full")
@@ -9,10 +10,22 @@ function push(newVal) {
     data[currentSize] = newVal;
     currentSize++;
 }
+// Remove Last Element 
+function pop() {
+    if (currentSize > 0) {
+        currentSize--;
+        data.length = currentSize;
+    } else {
+        console.log("Stack is Alrady Empty")
+    }
+}
+
 push(10);
 push(20);
 push(30);
 push(40);
 push(50);
+pop();
+pop();
 
 console.log(data);
