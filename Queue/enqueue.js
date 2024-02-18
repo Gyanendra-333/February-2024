a9e684e1e01681a5f552f1679fe8e3d772b3935d
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-02-18 22:47:49 +0530

    deQueue

--- Queue/enqueue.js
+++ Queue/enqueue.js
@@ -4,8 +4,34 @@ let queueSize = queue.length;
 let maxSize = 5;
 
 function enQueue(newVal) {
-    queue[queueSize] = newVal;
-    queueSize++;
+    if (queueSize >= maxSize) {
+        console.log("Queue is Already Full")
+    } else {
+        queue[queueSize] = newVal;
+        queueSize++;
+    }
 }
+function dispaly() {
+    console.log(queue);
+}
+function deQueue() {
+    if (queueSize > 0) {
+        for (let i = 0; i < queue.length; i++) {
+            queue[i] = queue[i + 1];
+        }
+        queueSize--;
+        queue.length = queueSize;
+    } else {
+        console.log("Queue is already Empty")
+    }
+
+}
+
 enQueue(10);
-console.log(queue);
+enQueue(20);
+enQueue(30);
+enQueue(40);
+deQueue();
+deQueue();
+
+dispaly();
