ec0bdb6ebb4aed16687feffb9d65fba476982a99
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-02-21 22:32:21 +0530

    Check Palindrome

---
+++ String/palimdrome.js
@@ -0,0 +1,20 @@
+// Level => leveL is palindrome.
+
+function palindrome(data) {
+    let start = 0;
+    let end = data.length - 1;
+    let result = true;
+
+    while (end > start) {
+        if (data[start] != data[end]) {
+            result = false;
+        }
+        start++;
+        end--;
+    }
+    return result;
+}
+let str = "level";
+console.log(palindrome(str));
+
+
