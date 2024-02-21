a73bb6479517508bd93242f24721f41f5ee89858
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-02-21 18:07:51 +0530

    Anagram

---
+++ String/anagram.js
@@ -0,0 +1,9 @@
+function checkAnagram(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    } else {
+        console
+    }
+}
+
+console.log(checkAnagram("Hello", "lloeh1"));
